c6409a155790cd8dea04200f69c730246a67d0e6
abulaiz <ronabulaiz@gmail.com>
2018-10-12 21:05:59 +0700

    Cek

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -7,6 +7,12 @@ use App\Libs\XLSXWriter;
 
 class ExcelController extends Controller
 {
+
+	protected $headerCol = array( 'font'=>'Tahoma','font-size'=>10,'font-style'=>'bold', 'fill'=>'#a29d9d', 'halign'=>'center','valign'=>'center', 'border'=>'left,right,top,bottom', 'height'=>20);
+	protected $header = array( 'font'=>'Tahoma','font-size'=>10,'font-style'=>'bold', 'halign'=>'left', 'valign'=>'center', 'height'=>20);	
+	protected $content = array( 'font'=>'Tahoma','font-size'=>10,'halign'=>'center', 'valign'=>'center', 'height'=>19, 'border'=>'left,right,top,bottom');		
+	protected $blank = array('height'=>20);		
+
     public function index(){
 		ini_set('display_errors', 0);
 		ini_set('log_errors', 1);
@@ -20,15 +26,24 @@ class ExcelController extends Controller
 		header('Cache-Control: must-revalidate');
 		header('Pragma: public');
 
-		$rows = array(
-		    array('2003','1','-50.5','2010-01-01 23:00:00','2012-12-31 23:00:00'),
-		    array('2003','=B2', '23.5','2010-01-01 00:00:00','2012-12-31 00:00:00'),
-		);
-
 		$writer = new XLSXWriter();
-		$writer->setAuthor('Some Author'); 
-		foreach($rows as $row)
-			$writer->writeSheetRow('Sheet1', $row);
+		$writer->setAuthor('Some Author');
+		$writer->writeSheetRow('Sheet1',$this->blankLine(), $this->blank);
+		$writer->writeSheetRow('Sheet1', array('ACARA 201 : 100M GAYA BEBAS S11 PUTRA'), $this->header);
+		$writer->writeSheetRow('Sheet1', array('ACARA 201 : 100M GAYA BEBAS S11 PUTRA'), $this->header);		
+		$writer->markMergedCell('Sheet1', 1,0, 1,6);
+		$writer->markMergedCell('Sheet1', 2,0, 2,6);
+		$writer->writeSheetRow('Sheet1', array(
+			'RANK', 'NAME', 'TGL LAHIR', 'KELAS', 'PROVINSI', 'HASIL', 'MEDALI'
+		), $this->headerCol);	
+		$writer->writeSheetRow('Sheet1', array(
+			1, 'Imron', 'Bebas', 'Berat', 'Jbar', 'Mana Saja', 'Emas'
+		), $this->content);				
     	return $writer->writeToStdOut();
     }
+
+    private function blankLine(){
+    	return array('','','','','','','');
+    }
+
 }
